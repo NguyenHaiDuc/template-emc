74b2dac86976f5e80711e42b64b938047dd556ec
Viet Bao <baotranviet123ht@gmail.com>
2020-04-08 22:06:56 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+@section('main')
+    @include('partials.slide')
+    @include('partials.category')
+    @include('partials.hot_product')
+    @include('partials.gallery')
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,8 @@
+@include('partials.header')
+   <body>
+        <main>
+            @yield('main')
+        </main>
+        @include('partials.footer')
+    </body>
+@include('partials.script_footer')
