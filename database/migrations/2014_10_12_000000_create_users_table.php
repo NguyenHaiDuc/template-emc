2dedee2429205958df5ce41c9c2b92221709ca57
NguyenHaiDuc <fantasy11230@gmail.com>
2020-04-12 14:17:33 +0700

    finish auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('gender')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('address');
+            $table->string('phone');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
